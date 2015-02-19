fd3544e17e8c3e4acae88f12e5747792c4e625a6
yaniv kessler <yanivk@gmail.com>
2015-02-19 15:25:52 +0200

    space

--- nwapp/lib/config.js
+++ nwapp/lib/config.js
@@ -2,6 +2,7 @@ var rc = require('rc')
 var packageJson = require('../package.json')
 var minimist = require('minimist')
 var argv = require('./argv.js')
+
 var defaults = {		
 	debug: false,
 	appName: packageJson.name,
