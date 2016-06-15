8c6a601fe6f119fea32d760cb33390b2e4d4c0be
yaniv kessler <yanivk@gmail.com>
2016-06-16 02:12:23 +0300

    update platforms, upgrade nwjs version

--- ninjaConfig.js
+++ ninjaConfig.js
@@ -6,9 +6,9 @@ var rc = require('rc')
 module.exports = rc('nw-ninja', {
 	appDir: './nwapp',
 	files: './nwapp/**/**', // use the glob format
-    platforms: [ 'osx', 'win' ],
-    buildDir: './dist',
-    buildType: 'versioned',
-    version: 'v0.12.3',
-    argv: process.argv.slice(2)
+	platforms: ['osx64', 'win32', 'win64', 'linux32', 'linux64'],
+	buildDir: './dist',
+	buildType: 'versioned',
+	version: 'v0.14.6',
+	argv: process.argv.slice(2)
 })
